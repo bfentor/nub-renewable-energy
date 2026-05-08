d1422201459423e9299c6a1fd2183af7c654eefe
bfentor <fentorb24@gmail.com>
2026-05-08 10:20:41 +0200

    damnit 2

--- src/main/scala/AnalysisEngine.scala
+++ src/main/scala/AnalysisEngine.scala
@@ -1,44 +1,41 @@
 //calculates energy data statistics
 object AnalysisEngine {
-  
-  def mean(data: List[Double]): Double = {
-    if (data.isEmpty) 0.0
-    else data.sum / data.length
-  }
-
-  def median(data: List[Double]): Double = {
-    if (data.isEmpty) 0.0
-    else {
-      val sorted = data.sorted
-      val n = sorted.length
-      if (n % 2 == 0) (sorted(n / 2 - 1) + sorted(n / 2)) / 2
-      else sorted(n / 2)
+    def mean(data: List[Double]): Double = {
+        val sum = data.sum
+        val count = data.length
+        if (count > 0) sum / count else 0.0
     }
-  }
 
-  def mode(data: List[Double]): Option[Double] = {
-    if (data.isEmpty) None
-    else {
-      val grouped = data.groupBy(identity).mapValues(_.length)
-      val maxCount = grouped.values.max
-      val modes = grouped.filter(_._2 == maxCount).keys.toList
-      if (modes.nonEmpty) Some(modes.head) else None
+    def median(data: List[Double]): Double = {
+        val sortedData = data.sorted
+        val count = sortedData.length
+        if (count == 0) return 0.0
+        if (count % 2 == 1) {
+            sortedData(count / 2)
+        } else {
+            (sortedData(count / 2 - 1) + sortedData(count / 2)) / 2.0
+        }
     }
-  }
 
+    def mode(data: List[Double]): Double = {
+        if (data.isEmpty) return 0.0
+        val frequencyMap = data.groupBy(identity).mapValues(_.length)
+        frequencyMap.maxBy(_._2)._1
+    }
+
+    // Difference between lowest and highest energy value
   def range(data: List[Double]): Double = {
-    if (data.isEmpty) 0.0
+    if (data.isEmpty) return 0.0
     else {
-      val sorted = data.sorted
-      sorted.last - sorted.head
-    }
+        val sorted = data.sorted
+        sorted.last - sorted.head
+        }
   }
 
+  // halfway point between minimum and maximum energy value
   def midrange(data: List[Double]): Double = {
-    if (data.isEmpty) 0.0
-    else {
-      val sorted = data.sorted
-      (sorted.head + sorted.last) / 2
-    }
+    if (data.isEmpty) return 0.0
+    val sorted = data.sorted
+    (sorted.head + sorted.last) / 2
   }
 }
